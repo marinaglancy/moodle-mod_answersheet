a3d38c7c9097c15aad8396cda88009abbe2da91b
Marina Glancy <marina@moodle.com>
2015-03-15 15:42:34 +0800

    reports

---
+++ classes/report.php
@@ -0,0 +1,76 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ *
+ *
+ * @package    mod_answersheet
+ * @copyright  2015 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ *
+ *
+ * @package    mod_answersheet
+ * @copyright  2015 Marina Glancy
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_answersheet_report {
+    public static function display($cm, $answersheet) {
+        global $OUTPUT;
+        $attempts = self::get_all_attempts($cm, $answersheet);
+        if (!$attempts) {
+            return '';
+        }
+        $table = new html_table();
+        $table->head = array(
+            'Student', // TODO strings
+            'Started',
+            'Grade',
+            'Answers'
+        );
+        foreach ($attempts as $attempt) {
+            $url = new moodle_url('/mod/answersheet/view.php', array('id' => $cm->id, 'attempt' => $attempt->id));
+            $table->data[] = array(fullname($attempt),
+                html_writer::link($url, userdate($attempt->timestarted, get_string('strftimedatetime', 'core_langconfig'))),
+                round($attempt->grade * 100, 2).'%',
+                $attempt->answers);
+        }
+        return $OUTPUT->heading('Completed attempts', 3). // TODO string
+            html_writer::table($table);
+    }
+
+    protected static function get_all_attempts($cm, $answersheet, $completedonly = true) {
+        global $DB;
+        $namefields = get_all_user_name_fields(true, 'u');
+        $records = $DB->get_records_sql(
+                'SELECT a.id, a.answers, a.timestarted, a.timecompleted, '.
+                'a.grade, a.userid, '.$namefields.' '.
+                'FROM {answersheet_attempt} a LEFT JOIN {user} u ON u.id = a.userid '.
+                'WHERE answersheetid = :aid '.
+                ($completedonly ? 'AND timecompleted IS NOT NULL ' : '').
+                'ORDER BY timestarted, id',
+                array('aid' => $answersheet->id));
+        $rv = array();
+        foreach ($records as $record) {
+            $rv[] = $record;
+        }
+        return $rv;
+    }
+}
